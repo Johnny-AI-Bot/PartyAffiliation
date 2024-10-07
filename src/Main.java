727a0aa5084201fef7ac21b5e64541f474027e65
jatur <turayjj@mail.uc.edu>
2024-10-07 11:56:34 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.util.Scanner;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String partyAfil = "";
+
+        System.out.println("Enter your party affiliation {R,D,I}: ");
+        partyAfil = in.nextLine();
+
+
+        if (partyAfil.equalsIgnoreCase("R")) {
+            System.out.println(" you get a Republican elephant");
+        }
+
+        else if (partyAfil.equalsIgnoreCase("D"))
+        {
+            System.out.println(" you get a Democrat Donkey");
+        }
+
+        else if (partyAfil.equalsIgnoreCase("I"))
+        {
+            System.out.println(" you get a Independent Man");
+        }
+        else
+
+       {
+        System.out.println("I don't know what party this is: " + partyAfil);
+        System.out.println("try again");
+       }
     }
 }
